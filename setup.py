eb7cddce8ea06979ad16121e0351b0a1566518eb
hazy <hajilumkun@gmail.com>
2024-05-05 10:52:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="mtprotocrypt",
-    version="1.2.6.5",
+    version="1.2.6.5b",
     description="Fast and Portable Cryptography Extension Library for Pyrogram",
     long_description=readme,
     long_description_content_type="text/markdown",
